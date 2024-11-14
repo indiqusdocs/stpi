44d8bff3c85709a7c11fe0b1b77da53b973d97db
indiqusdocs <yogesh.sharma@indiqus.com>
2024-11-14 11:01:24 +0530

    Updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ananta Technical Documentation',
+  title: 'Ananta Cloud Technical Documentation',
  // tagline: 'Find user guides, quickstarts, release notes, FAQs, and more for Service Providers and Subscribers.',
   favicon: 'img/favicon.ico',
 
